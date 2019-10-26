e0ba8c4578e0cc11928e0194e55ec2cb4933b044
Ryo Yasuda <e165729@ie.u-ryukyu.ac.jp>
2019-10-26 18:54:15 +0900

    joyconの傾きを同期する処理を追加

--- JPHACK2019/Assets/JoyconLib/JoyconLib_scripts/JoyconManager.cs
+++ JPHACK2019/Assets/JoyconLib/JoyconLib_scripts/JoyconManager.cs
@@ -1,15 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-
 using UnityEngine;
-using System;
-public class JoyconManager: MonoBehaviour
-{
+public class JoyconManager : MonoBehaviour {
 
-    // Settings accessible via Unity
-    public bool EnableIMU = true;
-    public bool EnableLocalize = true;
+	// Settings accessible via Unity
+	public bool EnableIMU = true;
+	public bool EnableLocalize = true;
 
 	// Different operating systems either do or don't like the trailing zero
 	private const ushort vendor_id = 0x57e;
@@ -17,87 +15,78 @@ public class JoyconManager: MonoBehaviour
 	private const ushort product_l = 0x2006;
 	private const ushort product_r = 0x2007;
 
-    public List<Joycon> j; // Array of all connected Joy-Cons
-    static JoyconManager instance;
+	public List<Joycon> j; // Array of all connected Joy-Cons
+	static JoyconManager instance;
 
-    public static JoyconManager Instance
-    {
-        get { return instance; }
-    }
+	public static JoyconManager Instance {
+		get { return instance; }
+	}
 
-    void Awake()
-    {
-        if (instance != null) Destroy(gameObject);
-        instance = this;
+	void Awake () {
+		if (instance != null) Destroy (gameObject);
+		instance = this;
 		int i = 0;
 
-		j = new List<Joycon>();
+		j = new List<Joycon> ();
 		bool isLeft = false;
-		HIDapi.hid_init();
+		HIDapi.hid_init ();
 
-		IntPtr ptr = HIDapi.hid_enumerate(vendor_id, 0x0);
+		IntPtr ptr = HIDapi.hid_enumerate (vendor_id, 0x0);
 		IntPtr top_ptr = ptr;
 
-		if (ptr == IntPtr.Zero)
-		{
-			ptr = HIDapi.hid_enumerate(vendor_id_, 0x0);
-			if (ptr == IntPtr.Zero)
-			{ 
-				HIDapi.hid_free_enumeration(ptr);
+		if (ptr == IntPtr.Zero) {
+			ptr = HIDapi.hid_enumerate (vendor_id_, 0x0);
+			if (ptr == IntPtr.Zero) {
+				HIDapi.hid_free_enumeration (ptr);
 				Debug.Log ("No Joy-Cons found!");
 			}
 		}
 		hid_device_info enumerate;
 		while (ptr != IntPtr.Zero) {
-			enumerate = (hid_device_info)Marshal.PtrToStructure (ptr, typeof(hid_device_info));
+			enumerate = (hid_device_info) Marshal.PtrToStructure (ptr, typeof (hid_device_info));
 
 			Debug.Log (enumerate.product_id);
-				if (enumerate.product_id == product_l || enumerate.product_id == product_r) {
-					if (enumerate.product_id == product_l) {
-						isLeft = true;
-						Debug.Log ("Left Joy-Con connected.");
-					} else if (enumerate.product_id == product_r) {
-						isLeft = false;
-						Debug.Log ("Right Joy-Con connected.");
-					} else {
-						Debug.Log ("Non Joy-Con input device skipped.");
-					}
-					IntPtr handle = HIDapi.hid_open_path (enumerate.path);
-					HIDapi.hid_set_nonblocking (handle, 1);
-					j.Add (new Joycon (handle, EnableIMU, EnableLocalize & EnableIMU, 0.05f, isLeft));
-					++i;
+			if (enumerate.product_id == product_l || enumerate.product_id == product_r) {
+				if (enumerate.product_id == product_l) {
+					isLeft = true;
+					Debug.Log ("Left Joy-Con connected.");
+				} else if (enumerate.product_id == product_r) {
+					isLeft = false;
+					Debug.Log ("Right Joy-Con connected.");
+				} else {
+					Debug.Log ("Non Joy-Con input device skipped.");
 				}
-				ptr = enumerate.next;
+				IntPtr handle = HIDapi.hid_open_path (enumerate.path);
+				HIDapi.hid_set_nonblocking (handle, 1);
+				j.Add (new Joycon (handle, EnableIMU, EnableLocalize & EnableIMU, 0.05f, isLeft));
+				++i;
 			}
+			ptr = enumerate.next;
+		}
 		HIDapi.hid_free_enumeration (top_ptr);
-    }
+	}
 
-    void Start()
-    {
-		for (int i = 0; i < j.Count; ++i)
-		{
-			Debug.Log (i);
-			Joycon jc = j [i];
+	void Start () {
+		for (int i = 0; i < j.Count; ++i) {
+			Joycon jc = j[i];
 			byte LEDs = 0x0;
-			LEDs |= (byte)(0x1 << i);
+			LEDs |= (byte) (0x1 << i);
 			jc.Attach (leds_: LEDs);
 			jc.Begin ();
+			jc.num = i;
+			Debug.Log (jc.num);
 		}
-    }
+	}
 
-    void Update()
-    {
-		for (int i = 0; i < j.Count; ++i)
-		{
-			j[i].Update();
+	void Update () {
+		for (int i = 0; i < j.Count; ++i) {
+			j[i].Update ();
 		}
-    }
+	}
 
-    void OnApplicationQuit()
-    {
-		for (int i = 0; i < j.Count; ++i)
-		{
+	void OnApplicationQuit () {
+		for (int i = 0; i < j.Count; ++i) {
 			j[i].Detach ();
 		}
-    }
+	}
 }
---
+++ JPHACK2019/Assets/JoyconLib/JoyconLib_scripts/JoyconSword.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyconSword : MonoBehaviour {
+    private static readonly Joycon.Button[] m_buttons =
+        Enum.GetValues (typeof (Joycon.Button)) as Joycon.Button[];
+
+    // joyconが格納されているリスト
+    private List<Joycon> m_joycons;
+    // joycon
+    private Joycon m_joyconL;
+    private Joycon m_joyconR;
+    // joyconのボタン取得
+    private Joycon.Button? m_pressedButtonL;
+    private Joycon.Button? m_pressedButtonR;
+
+    private Rigidbody rigidbody;
+
+    private void GetJoyconInput () {
+
+    }
+
+    // Start is called before the first frame update
+    void Start () {
+        m_joycons = JoyconManager.Instance.j;
+
+        if (m_joycons == null || m_joycons.Count <= 0) return;
+
+        m_joyconL = m_joycons.Find (c => c.isLeft);
+        m_joyconR = m_joycons.Find (c => !c.isLeft);
+
+        rigidbody = GetComponent<Rigidbody> ();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        Vector3 accel = m_joyconL.GetAccel ();
+        Vector3 gyro = m_joyconL.GetGyro ();
+        Quaternion orientation = m_joyconL.GetVector ();
+        Vector3 euler = orientation.eulerAngles;
+        orientation = new Quaternion (orientation.y, -orientation.z, -orientation.x, orientation.w);
+        Debug.Log ("acc:" + accel + " gyro:" + gyro + " ori:" + euler);
+        transform.rotation = orientation;
+
+        
+        //rigidbody.AddTorque (gyro);
+    }
+}
