c9587aed63272f3187b0f4cf9c98b83358c5bd11
boxboo <kazushi593641@gmail.com>
2019-10-26 14:29:06 +0900

    ゲームディレクターを追加しました。

---
+++ JPHACK2019/Assets/susiebi/Script/GameDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    [HideInInspector] public bool isBrokenTarget;
+    [HideInInspector] public bool gameFiniehd;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void DetectsTarget()
+    {
+        if(isBrokenTarget!=false) gameFiniehd=true;
+    }
+
+
+}
+
