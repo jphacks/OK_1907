13e7c733dd505c35e1435306b24f62d3a6baa2d6
bowwow-grr <std8zipped@gmail.com>
2019-10-26 17:27:58 +0900

    ターゲットの追加

---
+++ JPHACK2019/Assets/itokazu/target.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+/*   void Update()
+        Destroy (this.gameObject, 1.0f);
+    }
+*/
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Sword") 
+        {
+        Destroy(this.gameObject);
+        }
+    } 
+   
+}
