a4db888e964b950819e8aa5f02386ca3d931f0be
boxboo <kazushi593641@gmail.com>
2019-10-26 20:05:00 +0900

    プレイヤースクリプトを追加。

---
+++ JPHACK2019/Assets/susiebi/Script/Player/ChildrenB_Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildrenB_Player : ParentPlayer
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
